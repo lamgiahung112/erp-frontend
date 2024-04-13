96e9543d8847a0ccae72497b32cfb704ade6b7ef
Hung <Lam>
2024-04-13 23:33:41 +0700

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{ts,tsx}", "./index.html"],
+	theme: {
+		colors: {
+			concard: "#2A2A2A",
+		},
+		extend: {},
+	},
+	plugins: [],
+}
